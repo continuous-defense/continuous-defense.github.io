e6817299eee0aa41fe771cdbf97edfa4fc0cff5a
David Stevens <davidstevens37@gmail.com>
2015-12-20 01:33:16 -0700

    demo

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,9 @@
-var secret_sequence = [49, 57, 49, 49];
-var keys_pressed = [];
 
-
-function showContent() {
-	document.getElementById('hide-everything').remove();
+function hideContent() {
+	document.getElementsByTagName('html')[0].style.display = 'none';
 }
 
-document.addEventListener('keypress', function(e) {
 
-	if (e.keyCode === 13) {
-		var is_authorized = secret_sequence.join('|') === keys_pressed.join('|');
-		console.log(secret_sequence.join('|'), keys_pressed.join('|'))
-		keys_pressed = [];
-
-		console.log(is_authorized)
-		if (is_authorized) {
-			showContent();
-		}
-	} else {
-		keys_pressed.push(e.keyCode);
-	}
-});
+if (location.search !== '?demo=true') {
+	hideContent();
+}
