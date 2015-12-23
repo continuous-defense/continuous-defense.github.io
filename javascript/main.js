fc8a8b1a5e11316f6c4d73b98459e3d37bb5c105
David Stevens <davidstevens37@gmail.com>
2015-12-23 00:01:49 -0700

    added demo flag

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,23 @@ function hideContent() {
 	document.getElementsByTagName('html')[0].className = "under-construction"
 }
 
-
-if (location.search !== '?demo=true') {
+if (!localStorage) {
 	hideContent();
+} else {
+
+	if (location.search === '?demo=true') {
+		localStorage.demo_timestamp = new Date().getTime();
+	} else if (location.search === '?demo=false') {
+		localStorage.demo_timestamp = 0;
+	}
+
+	var timestamp = localStorage.demo_timestamp;
+	var now = new Date().getTime();
+	var difference = now - timestamp;
+	var one_hour = 60000 * 60;
+
+	if (!timestamp || difference > one_hour) {
+		hideContent();
+	}
+
 }
